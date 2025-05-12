6c28c6b74f00de4ff2caae239484e8b2ee9a444a
Huyen Tran <tranhuyenbk96@gmail.com>
2025-05-13 01:31:21 +0700

    remove key

--- service/api.ts
+++ service/api.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 const searchImages = async (term: string) => {
-  const response = await axios.get(`https://api.unsplash.com/search/photos`, {
-    headers: {
-      Authorization: "Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y",
-    },
-    params: {
-      query: term,
-    },
-  });
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_BASE_URL}/search/photos`,
+    {
+      headers: {
+        Authorization: process.env.REACT_APP_API_KEY || "",
+      },
+      params: {
+        query: term,
+      },
+    }
+  );
 
   return response.data.results;
 };
