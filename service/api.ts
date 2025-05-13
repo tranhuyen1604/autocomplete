16f6d4e989ab9515aac1c7d3a57074ab4048b26d
Huyen Tran <tranhuyenbk96@gmail.com>
2025-05-13 21:28:56 +0700

    fix: config

--- service/api.ts
+++ service/api.ts
@@ -2,17 +2,14 @@ import axios from "axios";
 import config from "../config";
 
 const searchImages = async (term: string) => {
-  const response = await axios.get(
-    `${config.unsplash}/search/photos`,
-    {
-      headers: {
-        Authorization: process.env.REACT_APP_API_KEY || "",
-      },
-      params: {
-        query: term,
-      },
-    }
-  );
+  const response = await axios.get(`${config.unsplash}/search/photos`, {
+    headers: {
+      Authorization: import.meta.env.REACT_APP_API_KEY,
+    },
+    params: {
+      query: term,
+    },
+  });
 
   return response.data.results;
 };
